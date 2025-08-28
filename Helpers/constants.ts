96411c8b2817fffb6862812acc5d58940e000c46
oebelus <imaneelfidrissi@hotmail.com>
2025-08-28 10:09:15 +0100

    better settings handling

--- Helpers/constants.ts
+++ Helpers/constants.ts
@@ -1,1 +1,104 @@
-export const ALL_WRAPPERS = [{"name":"Text Color","wrappers":[{"name":"Blue","content":"<span style=\"color: blue\">{{}}</span>"},{"name":"Yellow","content":"<span style=\"color: yellow\">{{}}</span>"},{"name":"Purple","content":"<span style=\"color: purple\">{{}}</span>"},{"name":"Green","content":"<span style=\"color: green\">{{}}</span>"},{"name":"Cyan","content":"<span style=\"color:cyan\">{{}}</span>"}]},{"name":"Background Color","wrappers":[{"name":"Red","content":"<span style=\"background-color: red\">{{}}</span>"},{"name":"Green","content":"<span style=\"background-color: green\">{{}}</span>"},{"name":"Blue","content":"<span style=\"background-color: blue\">{{}}</span>"},{"name":"Yellow","content":"<span style=\"background-color: yellow\">{{}}</span>"},{"name":"Purple","content":"<span style=\"background-color: purple\">{{}}</span>"}]},{"name":"Outline","wrappers":[{"name":"Red","content":"<div style=\"border: 2px solid red; padding: 5px;\">{{}}</div>"},{"name":"Green","content":"<div style=\"border: 2px solid green; padding: 5px;\">{{}}</div>"},{"name":"Blue","content":"<div style=\"border: 2px solid blue; padding: 5px;\">{{}}</div>"},{"name":"Yellow","content":"<div style=\"border: 2px solid yellow; padding: 5px;\">{{}}</div>"},{"name":"Purple","content":"<div style=\"border: 2px solid purple; padding: 5px;\">{{}}</div>"}]},{"name":"Text Style","wrappers":[{"name":"Bold","content":"<strong>{{}}</strong>"},{"name":"Italic","content":"<em>{{}}</em>"},{"name":"Underline","content":"<u>{{}}</u>"},{"name":"Strikethrough","content":"<s>{{}}</s>"},{"name":"Superscript","content":"<sup>{{}}</sup>"},{"name":"Subscript","content":"<sub>{{}}</sub>"}]},{"name":"Code","wrappers":[{"name":"Inline Code","content":"<code>{{}}</code>"},{"name":"Code Block","content":"<pre><code>{{}}</code></pre>"}]},{"name":"Image Position","wrappers":[{"name":"Center","content":"<div style=\"display:flex; justify-content:center\"> <img src=\"{{}}\" /> </div>"}]}]
+export const ALL_WRAPPERS = [
+	{
+		name: "Text Color",
+		wrappers: [
+			{ name: "Blue", content: '<span style="color: blue">{{}}</span>' },
+			{
+				name: "Yellow",
+				content: '<span style="color: yellow">{{}}</span>',
+			},
+			{
+				name: "Purple",
+				content: '<span style="color: purple">{{}}</span>',
+			},
+			{
+				name: "Green",
+				content: '<span style="color: green">{{}}</span>',
+			},
+			{ name: "Cyan", content: '<span style="color:cyan">{{}}</span>' },
+		],
+	},
+	{
+		name: "Background Color",
+		wrappers: [
+			{
+				name: "Red",
+				content: '<span style="background-color: red">{{}}</span>',
+			},
+			{
+				name: "Green",
+				content: '<span style="background-color: green">{{}}</span>',
+			},
+			{
+				name: "Blue",
+				content: '<span style="background-color: blue">{{}}</span>',
+			},
+			{
+				name: "Yellow",
+				content: '<span style="background-color: yellow">{{}}</span>',
+			},
+			{
+				name: "Purple",
+				content: '<span style="background-color: purple">{{}}</span>',
+			},
+		],
+	},
+	{
+		name: "Outline",
+		wrappers: [
+			{
+				name: "Red",
+				content:
+					'<div style="border: 2px solid red; padding: 5px;">{{}}</div>',
+			},
+			{
+				name: "Green",
+				content:
+					'<div style="border: 2px solid green; padding: 5px;">{{}}</div>',
+			},
+			{
+				name: "Blue",
+				content:
+					'<div style="border: 2px solid blue; padding: 5px;">{{}}</div>',
+			},
+			{
+				name: "Yellow",
+				content:
+					'<div style="border: 2px solid yellow; padding: 5px;">{{}}</div>',
+			},
+			{
+				name: "Purple",
+				content:
+					'<div style="border: 2px solid purple; padding: 5px;">{{}}</div>',
+			},
+		],
+	},
+	{
+		name: "Text Style",
+		wrappers: [
+			{ name: "Bold", content: "<strong>{{}}</strong>" },
+			{ name: "Italic", content: "<em>{{}}</em>" },
+			{ name: "Underline", content: "<u>{{}}</u>" },
+			{ name: "Strikethrough", content: "<s>{{}}</s>" },
+			{ name: "Superscript", content: "<sup>{{}}</sup>" },
+			{ name: "Subscript", content: "<sub>{{}}</sub>" },
+		],
+	},
+	{
+		name: "Code",
+		wrappers: [
+			{ name: "Inline Code", content: "<code>{{}}</code>" },
+			{ name: "Code Block", content: "<pre><code>{{}}</code></pre>" },
+		],
+	},
+	{
+		name: "Image Position",
+		wrappers: [
+			{
+				name: "Center",
+				content:
+					'<div style="display:flex; justify-content:center"> <img src="{{}}" /> </div>',
+			},
+		],
+	},
+];
--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-import { ALL_WRAPPERS } from "Helpers/constants";
 import { HTMLWrapper, Wrapper } from "Helpers/interfaces";
 import { containsHTMLTags, stripHTMLTags } from "Helpers/htmlchecker";
 import { Editor, Menu, Notice, Plugin } from "obsidian";
@@ -11,7 +10,119 @@ interface HtmlWrapperSettings {
 }
 
 const DEFAULT_SETTINGS: HtmlWrapperSettings = {
-	wrappers: [],
+	wrappers: [
+		{
+			name: "Text Color",
+			wrappers: [
+				{
+					name: "Blue",
+					content: '<span style="color: blue">{{}}</span>',
+				},
+				{
+					name: "Yellow",
+					content: '<span style="color: yellow">{{}}</span>',
+				},
+				{
+					name: "Purple",
+					content: '<span style="color: purple">{{}}</span>',
+				},
+				{
+					name: "Green",
+					content: '<span style="color: green">{{}}</span>',
+				},
+				{
+					name: "Cyan",
+					content: '<span style="color:cyan">{{}}</span>',
+				},
+			],
+		},
+		{
+			name: "Background Color",
+			wrappers: [
+				{
+					name: "Red",
+					content: '<span style="background-color: red">{{}}</span>',
+				},
+				{
+					name: "Green",
+					content:
+						'<span style="background-color: green">{{}}</span>',
+				},
+				{
+					name: "Blue",
+					content: '<span style="background-color: blue">{{}}</span>',
+				},
+				{
+					name: "Yellow",
+					content:
+						'<span style="background-color: yellow">{{}}</span>',
+				},
+				{
+					name: "Purple",
+					content:
+						'<span style="background-color: purple">{{}}</span>',
+				},
+			],
+		},
+		{
+			name: "Outline",
+			wrappers: [
+				{
+					name: "Red",
+					content:
+						'<div style="border: 2px solid red; padding: 5px;">{{}}</div>',
+				},
+				{
+					name: "Green",
+					content:
+						'<div style="border: 2px solid green; padding: 5px;">{{}}</div>',
+				},
+				{
+					name: "Blue",
+					content:
+						'<div style="border: 2px solid blue; padding: 5px;">{{}}</div>',
+				},
+				{
+					name: "Yellow",
+					content:
+						'<div style="border: 2px solid yellow; padding: 5px;">{{}}</div>',
+				},
+				{
+					name: "Purple",
+					content:
+						'<div style="border: 2px solid purple; padding: 5px;">{{}}</div>',
+				},
+			],
+		},
+		{
+			name: "Text Style",
+			wrappers: [
+				{ name: "Bold", content: "<strong>{{}}</strong>" },
+				{ name: "Italic", content: "<em>{{}}</em>" },
+				{ name: "Underline", content: "<u>{{}}</u>" },
+				{ name: "Strikethrough", content: "<s>{{}}</s>" },
+				{ name: "Superscript", content: "<sup>{{}}</sup>" },
+				{ name: "Subscript", content: "<sub>{{}}</sub>" },
+			],
+		},
+		{
+			name: "Code",
+			wrappers: [
+				{ name: "Inline Code", content: "<code>{{}}</code>" },
+				{ name: "Code Block", content: "<pre><code>{{}}</code></pre>" },
+			],
+		},
+		{
+			name: "Image Position",
+			wrappers: [
+				{
+					name: "Center",
+					content:
+						'<div style="display:flex; justify-content:center"> <img src="{{}}" /> </div>',
+				},
+			],
+		},
+	],
 };
 
 export default class HtmlWrapper extends Plugin {
@@ -30,76 +141,27 @@ export default class HtmlWrapper extends Plugin {
 					this.app,
 					async (name, content, category) => {
 						try {
-							const filename = "constants.ts";
-
 							const html: Wrapper = {
 								name: name,
 								content: content,
 							};
 
-							const pluginDirectory =
-								this.app.vault.configDir +
-								"/plugins/html-wrapper-plugin/Helpers/";
-							const filePath = pluginDirectory + filename;
-							console.log("file", filePath)
-
-							const fileExists =
-								await this.app.vault.adapter.exists(filePath);
-
-							console.log(fileExists, "hereeeee");
-
-							if (fileExists) {
-								const categoryExists = ALL_WRAPPERS.find(
-									(wrapper: HTMLWrapper) => {
-										return (
-											wrapper.name.trim() ==
-											category.trim()
-										);
-									}
+							const existingCategory =
+								this.settings.wrappers.find(
+									(wrapper) =>
+										wrapper.name.trim() === category.trim()
 								);
-								if (
-									categoryExists !== undefined &&
-									(categoryExists as HTMLWrapper).wrappers
-										.length > 0
-								) {
-									ALL_WRAPPERS.forEach(
-										(wrapper: HTMLWrapper) => {
-											if (wrapper.name === category) {
-												wrapper.wrappers.push(html);
-											}
-										}
-									);
 
-									console.log(ALL_WRAPPERS);
-								} else {
-									(
-										ALL_WRAPPERS as unknown as HTMLWrapper[]
-									).push({
-										name: category,
-										wrappers: [html],
-									});
-								}
+							if (existingCategory) {
+								existingCategory.wrappers.push(html);
 							} else {
-								await this.app.vault.create(
-									filePath,
-									"export const ALL_WRAPPERS = []"
-								);
-
-								(ALL_WRAPPERS as unknown as HTMLWrapper[]).push(
-									{
-										name: category,
-										wrappers: [html],
-									}
-								);
-								return;
+								this.settings.wrappers.push({
+									name: category,
+									wrappers: [html],
+								});
 							}
 
-							await this.app.vault.adapter.write(
-								filePath,
-								"export const ALL_WRAPPERS = " +
-									JSON.stringify(ALL_WRAPPERS)
-							);
-
+							await this.saveSettings();
 							new Notice("HTML wrapper created successfully!");
 						} catch (error) {
 							new Notice("Error creating HTML wrapper: " + error);
@@ -125,90 +187,94 @@ export default class HtmlWrapper extends Plugin {
 							// @ts-ignore
 							const categorySubmenu = item.setSubmenu();
 
-							ALL_WRAPPERS.forEach((category: HTMLWrapper) => {
-								categorySubmenu.addItem(
-									(subitem: HTMLWrapper) => {
-										subitem
-											// @ts-ignore
-											.setTitle(category.name)
-											.setIcon("folder");
+							this.settings.wrappers.forEach(
+								(category: HTMLWrapper) => {
+									categorySubmenu.addItem(
+										(subitem: HTMLWrapper) => {
+											subitem
+												// @ts-ignore
+												.setTitle(category.name)
+												.setIcon("folder");
 
-										const wrapperSubmenu =
-											// @ts-ignore
-											subitem.setSubmenu();
-										category.wrappers.forEach(
-											(wrapper: Wrapper) => {
-												wrapperSubmenu.addItem(
-													(wrapperItem: Wrapper) => {
-														wrapperItem
-															// @ts-ignore
-															.setTitle(
-																wrapper.name
-															)
-															.setIcon("code")
-															.onClick(() => {
-																if (
-																	containsHTMLTags(
-																		selectedText
-																	)
+											const wrapperSubmenu =
+												// @ts-ignore
+												subitem.setSubmenu();
+											category.wrappers.forEach(
+												(wrapper: Wrapper) => {
+													wrapperSubmenu.addItem(
+														(
+															wrapperItem: Wrapper
+														) => {
+															wrapperItem
+																// @ts-ignore
+																.setTitle(
+																	wrapper.name
 																)
-																	new StripHtmlModal(
-																		this.app,
-																		(
-																			shouldStrip: boolean
-																		) => {
-																			try {
-																				if (
-																					shouldStrip
-																				) {
-																					const html =
-																						wrapper.content.replace(
-																							"{{}}",
-																							stripHTMLTags(
+																.setIcon("code")
+																.onClick(() => {
+																	if (
+																		containsHTMLTags(
+																			selectedText
+																		)
+																	)
+																		new StripHtmlModal(
+																			this.app,
+																			(
+																				shouldStrip: boolean
+																			) => {
+																				try {
+																					if (
+																						shouldStrip
+																					) {
+																						const html =
+																							wrapper.content.replace(
+																								"{{}}",
+																								stripHTMLTags(
+																									selectedText
+																								)
+																							);
+																						editor.replaceSelection(
+																							html
+																						);
+																					} else {
+																						const html =
+																							wrapper.content.replace(
+																								"{{}}",
 																								selectedText
-																							)
+																							);
+																						editor.replaceSelection(
+																							html
 																						);
-																					editor.replaceSelection(
-																						html
-																					);
-																				} else {
-																					const html =
-																						wrapper.content.replace(
-																							"{{}}",
-																							selectedText
-																						);
-																					editor.replaceSelection(
-																						html
+																					}
+																				} catch (error) {
+																					new Notice(
+																						"Error wrapping HTML: " +
+																							error
 																					);
 																				}
-																			} catch (error) {
-																				new Notice(
-																					"Error wrapping HTML: " +
-																						error
-																				);
 																			}
-																		}
-																	).open();
-																else {
-																	const html =
-																		wrapper.content.replace(
-																			"{{}}",
-																			selectedText
+																		).open();
+																	else {
+																		const html =
+																			wrapper.content.replace(
+																				"{{}}",
+																				selectedText
+																			);
+																		editor.replaceSelection(
+																			html
 																		);
-																	editor.replaceSelection(
-																		html
-																	);
-																}
-															});
-													}
-												);
-											}
-										);
-										// @ts-ignore
-										subitem.setSubmenu(wrapperSubmenu);
-									}
-								);
-							});
+																	}
+																});
+														}
+													);
+												}
+											);
+											// @ts-ignore
+											subitem.setSubmenu(wrapperSubmenu);
+										}
+									);
+								}
+							);
 
 							// @ts-ignore
 							item.setSubmenu(categorySubmenu);
